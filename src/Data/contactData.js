397d4cfbb0ed4d7d0beddd33679d798b4ef6c22e
aksagar2206 <aksagar22@gmail.com>
2025-08-12 15:20:27 +0530

    Update contactData.js

--- src/Data/contactData.js
+++ src/Data/contactData.js
@@ -1,18 +1,18 @@
 export const contactData=[
     [
-        ["Prof. Parma Nanad, Sharda University","parmaastya@gmail.com,"," "],
-        ["Prof. Anil Kumar Sagar, Sharda University","aksagar22@gmail.com,","conference@sirsd.com"]
+        ["Prof. Parma Nand, Sharda University","parmaastya@gmail.com"," "],
+        ["Prof. Anil Kumar Sagar, Sharda University","aksagar22@gmail.com,",""]
     ],
     [
-        ["Prof. Rajashree Adhikary, Sharda University","sibanifoundation@gmail.com,","conference@sirsd.com"],
-        ["Prof. Rajashree Adhikary, Sharda University","sibanifoundation@gmail.com,","conference@sirsd.com"]
+        ["Prof.  Anil Kumar Sagar, Sharda University","aksagar22@gmail.com,",""],
+        ["Prof.  Anil Kumar Sagar, Sharda University","aksagar22@gmail.com,",""]
     ],
     [
-        ["Prof. Rajashree Adhikary, Sharda University","sibanifoundation@gmail.com,","conference@sirsd.com"],
-        ["Prof. Rajashree Adhikary, Sharda University","sibanifoundation@gmail.com,","conference@sirsd.com"]
+        ["Prof.  Anil Kumar Sagar, Sharda University","aksagar22@gmail.com,",""],
+        ["Prof.  Anil Kumar Sagar, Sharda University","aksagar22@gmail.com,",""]
     ],
     [
-        ["Prof. Rajashree Adhikary, Sharda University","sibanifoundation@gmail.com,","conference@sirsd.com"],
-        ["Prof. Rajashree Adhikary, Sharda University","sibanifoundation@gmail.com,","conference@sirsd.com"]
+        ["Prof.  Anil Kumar Sagar, Sharda University","aksagar22@gmail.com,",""],
+        ["Prof.  Anil Kumar Sagar, Sharda University","aksagar22@gmail.com,",""]
     ]
 ]
