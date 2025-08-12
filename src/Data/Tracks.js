78ac49426a9def6372adffd31d7b69bd46e3db72
aksagar2206 <aksagar22@gmail.com>
2025-08-12 15:36:59 +0530

    Update Tracks.js

--- src/Data/Tracks.js
+++ src/Data/Tracks.js
@@ -1,14 +1,10 @@
 export const tracks={
     head:"The conference will cover a wide range of topics related to Smart Cities and Urban Development, Artificial Intelligence and Machine Learning, Advance Data Communication and Edge Computing,Renewable Energy and Smart Grids, Robotics Automation and Networking, Digital Health and Smart Health Informatics:",
     tracks:[{
-        head:"Smart Cities and Urban Development",
+        head:"Next-Gen Computing: AI-Driven Innovations and Smart Technologies",
         data:[
-            "Smart infrastructure and intelligent transportation systems",
-            "Urban IoT and real-time city data",
-            "Energy-efficient smart buildings",
-            "Data-Driven Urban Planning",
-            "Technological Integration in Cities",
-            "Artificial Intelligence in City Management"
+            "AI and Machine Learning Innovations, Smart Technologies and IoT, Big Data Analytics and Cloud Computing, Cybersecurity and AI-Enabled Threat Detection, AI in Computer Vision and Robotics, AI for Software Engineering and Development, AI in Healthcare and Bioinformatics, AI in Emerging Computing Paradigms",
+            
         ]
     },
     {
