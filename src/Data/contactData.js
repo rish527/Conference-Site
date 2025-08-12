d6a88863c3221bcc429f8338c1e4b2218ccb927b
aksagar2206 <aksagar22@gmail.com>
2025-08-12 15:17:27 +0530

    Update contactData.js

--- src/Data/contactData.js
+++ src/Data/contactData.js
@@ -1,7 +1,7 @@
 export const contactData=[
     [
-        ["Prof. Parma Nanad, Sharda University","sibanifoundation@gmail.com,","conference@sirsd.com"],
-        ["Prof. Rajashree Adhikary, Sharda University","sibanifoundation@gmail.com,","conference@sirsd.com"]
+        ["Prof. Parma Nanad, Sharda University","parmaastya@gmail.com,"," "],
+        ["Prof. Anil Kumar Sagar, Sharda University","aksagar22@gmail.com,","conference@sirsd.com"]
     ],
     [
         ["Prof. Rajashree Adhikary, Sharda University","sibanifoundation@gmail.com,","conference@sirsd.com"],
