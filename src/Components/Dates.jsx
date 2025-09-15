bd8314ce3fb5fbdc29eecffa7705c614bda36603
aksagar2206 <aksagar22@gmail.com>
2025-09-15 10:18:13 +0530

    Update Dates.jsx

--- src/Components/Dates.jsx
+++ src/Components/Dates.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 function Dates() {
   return (
     <div className="dates">
-        <p>Paper Submission Deadline: <span>15th November, 2025</span></p>
-        <p>Notification of Acceptance: <span>5th December, 2025</span></p>
-        <p>Registration/ Camera Ready Deadline: <span>5th January, 2026</span></p>
-        <p>Conference Dates: <span>24th - 25th January, 2026</span></p>
+        <p>Paper Submission Deadline: <span>30th November, 2025</span></p>
+        <p>Notification of Acceptance: <span>15th December, 2025</span></p>
+        <p>Registration/ Camera Ready Deadline: <span>15th January, 2026</span></p>
+        <p>Conference Dates: <span>20th - 21st February, 2026</span></p>
     </div>
     
   )
