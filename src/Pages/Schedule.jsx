b2fa0c11ea5c4f179370ad48e4f930f7d861cf2e
aksagar2206 <aksagar22@gmail.com>
2025-09-30 11:32:43 +0530

    Update Schedule.jsx

--- src/Pages/Schedule.jsx
+++ src/Pages/Schedule.jsx
@@ -8,7 +8,7 @@ function Schedule() {
         <MiniHero title="Schedule"/>
         <div className="mt-11 mb-11 mx-[80px]">
             <h1 className='uppercase text-[#31619Eff] pb-3  text-2xl'>Schedule</h1>
-            <p><a className='text-blue-800 underline' href="">Click here</a> to view nternational Conference on Emerging and Advanced Engineering Technologies - ICEAET-2026 on 18th & 19th April 2026 schedule.</p>
+            <p><a className='text-blue-800 underline' href="">Click here</a> to view international Conference on Emerging Engineering Technologies and Applications ICEETA-2026 on 20th & 21th February 2026 schedule.</p>
         </div>
         <br />
         <Footer />
