771df706b645806670d682f5568d624b4eaf96f0
aksagar2206 <aksagar22@gmail.com>
2025-10-09 17:08:53 +0530

    Update contactData.js

--- src/Data/contactData.js
+++ src/Data/contactData.js
@@ -1,7 +1,7 @@
 export const contactData=[
     [
-        ["Prof.  Dr Sanjay Das, Convenor ICEETA 2026","Ph. 9289043931","iceetaigntu@gmail.com"],
-        ["Prof.  Anil Kumar Sagar, Co-Convenor ICEETA 2026", "Ph. 7906839179","iceetaigntu@gmail.com"]
+        ["Prof.  Dr Sanjay Das, Convenor ICEETA 2026"," 9289043931","iceetaigntu@gmail.com"],
+        ["Prof.  Anil Kumar Sagar, Co-Convenor ICEETA 2026", " 7906839179","iceetaigntu@gmail.com"]
     ],
     [
         ["Prof. Parma Nand, Sharda University","parmaastya@gmail.com"," "],
