f1d7803d498c436794e6e31e39ce24e97dc7b47d
aksagar2206 <aksagar22@gmail.com>
2025-10-09 13:38:00 +0530

    Update Submission.jsx

--- src/Pages/Submission.jsx
+++ src/Pages/Submission.jsx
@@ -56,6 +56,7 @@ function Submission() {
                         }
                     </ul>
                   <br/>
+                  <p className='font-bold mb-2'>CMT Acknowledgement</p>
                   <p>The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.</p>
 
 
