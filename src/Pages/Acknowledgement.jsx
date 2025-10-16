d59892b4954f03c1a33d0529cc45cae9bae8c14f
Asus <rishav032005@gmail.com>
2025-10-16 17:40:04 +0530

    made static

--- src/Pages/Acknowledgement.jsx
+++ src/Pages/Acknowledgement.jsx
@@ -3,10 +3,16 @@ import MiniHero from '../Components/MiniHero'
 import Footer from '../Components/Footer'
 
 
-function Venue() {
+function Acknowledgement() {
   return (
     <div>
-      <MiniHero title="CMT Acknowledgement" />
+      {/* <MiniHero title="CMT Acknowledgement" /> */}
+      <div id="mini-hero" className="w-full flex justify-center items-center overlay">
+          <div className="overlay"></div>
+          <div  className="content relative w-full container flex justify-center items-center h-60">
+              <h1>CMT Acknowledgement</h1>
+          </div>
+      </div>
       <div className="mt-11 mb-11 mx-[80px]">
         {/* <h1 className='uppercase text-[#31619Eff] pb-3  text-2xl'>CMT Acknowledgement</h1> */}
         <p>The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.</p>
@@ -20,4 +26,4 @@ function Venue() {
   )
 }
 
-export default Venue
+export default Acknowledgement;
