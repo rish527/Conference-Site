229fece7497e7d45837a595855be5038a1c3ee9a
aksagar2206 <aksagar22@gmail.com>
2025-10-16 13:53:58 +0530

    Update Acknowledgement.jsx

--- src/Pages/Acknowledgement.jsx
+++ src/Pages/Acknowledgement.jsx
@@ -10,8 +10,8 @@ function Venue() {
       <div className="mt-11 mb-11 mx-[80px]">
         {/* <h1 className='uppercase text-[#31619Eff] pb-3  text-2xl'>CMT Acknowledgement</h1> */}
         CMT ACKNOWLEDGMENT <br></br>
-        This conference uses Microsoft CMT for paper submission and review.
-        The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.
+        The Microsoft CMT service was used for managing the paper submission and peer-reviewing process for this conference. This service was provided free of charge by Microsoft, which covered all costs including those for Azure cloud services, software development, and support.
+
         <br />
       </div>
       
