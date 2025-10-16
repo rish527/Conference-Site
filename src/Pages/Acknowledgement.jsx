d75b7d2d317d130ca3aca266214c2c2d52a19a5e
aksagar2206 <aksagar22@gmail.com>
2025-10-16 13:40:11 +0530

    Update Acknowledgement.jsx

--- src/Pages/Acknowledgement.jsx
+++ src/Pages/Acknowledgement.jsx
@@ -10,7 +10,8 @@ function Venue() {
       <div className="mt-11 mb-11 mx-[80px]">
         {/* <h1 className='uppercase text-[#31619Eff] pb-3  text-2xl'>CMT Acknowledgement</h1> */}
         <h3><b>CMT Acknowledgement</b></h3>
-        "The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support."
+        This conference uses Microsoft CMT for paper submission and review.
+        The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.
         <br />
       </div>
       
